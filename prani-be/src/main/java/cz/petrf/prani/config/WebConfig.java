5e14979432985abea2266a58e2b5e37e579b127c
Petr Franta <petr.franta@gmail.com>
2025-12-29 02:22:33 +0100

    Upravena konfigurace pro SPA aplikaci

--- prani-be/src/main/java/cz/petrf/prani/config/WebConfig.java
+++ prani-be/src/main/java/cz/petrf/prani/config/WebConfig.java
@@ -6,12 +6,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/{spring:\\w+}")
-                .setViewName("forward:/index.html");
-        registry.addViewController("/**/{spring:\\w+}")
-                .setViewName("forward:/index.html");
-    }
+
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addViewController("/{path:[^\\.]*}")
+        .setViewName("forward:/index.html");
+    registry.addViewController("/{path:[^\\.]*}/**")
+        .setViewName("forward:/index.html");
+  }
 }
