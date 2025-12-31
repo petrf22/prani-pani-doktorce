464758815333ba68041d7008a37edd338040b4ad
Petr Franta <petr.franta@gmail.com>
2025-12-31 16:21:46 +0100

    Upraveny texty a mezery mezi elementy

--- prani-fe/src/app/uvod/uvod.component.ts
+++ prani-fe/src/app/uvod/uvod.component.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { MailComponent } from "../mail/mail.component";
 import { UserService } from '../services/user-service';
 import { RouterLink } from '@angular/router';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
 
 @Component({
   selector: 'app-uvod',
-  imports: [MailComponent, RouterLink],
+  imports: [MailComponent, RouterLink, NzSpaceModule],
   templateUrl: './uvod.component.html',
   styleUrl: './uvod.component.scss'
 })
